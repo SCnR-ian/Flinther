b0fe436e28422abe884a33ea744db132878a3f7d
SCnR-ian <kevin225025582@gmail.com>
2026-06-01 18:14:00 +1000

    Remove VitePWA: service worker causing Safari redirect errors

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    registerType: 'autoUpdate',
-    injectRegister: 'auto',
-    includeAssets: ['favicon.svg', 'icons/icon-192.png', 'icons/icon-512.png'],
-    workbox: {
-      skipWaiting: true,
-      clientsClaim: true,
-    },
-    manifest: {
-      name: 'Flinther',
-      short_name: 'Flinther',
-      description: 'Table tennis club management platform.',
-      start_url: '/',
-      display: 'standalone',
-      background_color: '#ffffff',
-      theme_color: '#000000',
-      icons: [
-        { src: '/icons/icon-192.png', sizes: '192x192', type: 'image/png' },
-        { src: '/icons/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' },
-      ],
-    },
-  })],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': '/src',
