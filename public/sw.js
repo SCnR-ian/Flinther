401b90742e8a89e176608c48ba0308fb39165db9
SCnR-ian <kevin225025582@gmail.com>
2026-06-01 18:15:55 +1000

    Add sw.js that self-unregisters old PWA service worker

---
+++ public/sw.js
@@ -0,0 +1,9 @@
+// Unregister service worker
+self.addEventListener('install', () => self.skipWaiting())
+self.addEventListener('activate', () => {
+  self.registration.unregister().then(() => {
+    self.clients.matchAll({ type: 'window' }).then(clients => {
+      clients.forEach(client => client.navigate(client.url))
+    })
+  })
+})
